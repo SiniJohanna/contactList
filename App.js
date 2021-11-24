53fcbfbe2bc852137036212329d5e207a489c7f7
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-11-24 19:07:28 +0200

    toimii

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
+
+
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const [currentContact, setCurrentContact] = useState({});
+
+  
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers]
+      })
+
+      setContacts(data);
+      if (data.length > 0) {
+        setCurrentContact(data[0]);
+        console.log(data[0]);
+      }
+    }
+  }
+  const buttonPressed = () => {
+    getContacts();
+  }
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={styles.container} >
       <StatusBar style="auto" />
+          <View >
+            <FlatList 
+              ListHeaderComponent= {<Text >Your contacts:</Text>}
+              data={contacts}
+              renderItem={({ item }) =>
+                <Text>{item.name} {item.phoneNumbers[0].number}</Text>
+              }
+            />
+            <Button onPress={buttonPressed} title="Get contacts"  />
+          </View>
     </View>
   );
 }
@@ -17,5 +58,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 100
+    
+    
   },
 });
